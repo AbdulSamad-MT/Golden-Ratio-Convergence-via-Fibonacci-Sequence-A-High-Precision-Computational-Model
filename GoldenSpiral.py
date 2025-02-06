1dd5a50e6c0c59d8762cb2084cebe3b42aed245a
AbdulSamad-MT <2240001093@iau.edu.sa>
2025-02-06 12:18:44 +0300

    GoldenSpiral.py

--- GoldenSpiral.py
+++ GoldenSpiral.py
@@ -3,22 +3,13 @@ from mpmath import mp
 mp.dps = int(input("how many floating points you want? "))
 
 def fibonacci_ratio(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    a, b = 0, 1
-    for _ in range(2, n + 1):
-        a, b = b, a + b
-    return b / a
+    F_n = mp.fibonacci(n)
+    F_n1 = mp.fibonacci(n+1)
+    return F_n1 / F_n
 
 def golden_ratio_fibonacci_approximation():
     n = 1000
     return fibonacci_ratio(n)
 
-
-
 phi_precise = golden_ratio_fibonacci_approximation()
-
 print(f"Golden Ratio using Fibonacci approximation with {mp.dps} decimal places:\n{phi_precise}")
-
